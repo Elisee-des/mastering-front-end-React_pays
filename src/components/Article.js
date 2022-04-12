58c80026f3852f5e39836c2376c92f087b50d2d5
Elisee-des <esabidani@gmail.com>
2022-04-12 00:49:55 +0000

    __Suppression des article.debut

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import { useState } from 'react';
 
 const Article = ({ article }) => {
+
+    const [isEditing, setIsEditing] = useState(false)
+    const [editContent, setEditContent] = useState("")
 
     const dataParser = (date) => {
         let newData = new Date(date).toLocaleDateString("fr-FR", {
@@ -13,15 +17,25 @@ const Article = ({ article }) => {
         return newData
     }
 
+    const handleEdit = () => {
+        setIsEditing(false)
+    }
+
     return (
         <div className="article">
             <div className="card-header">
                 <h3>{article.author}</h3>
                 <em>Posté le: {dataParser(article.date)}</em>
             </div>
-            <p>{article.content}</p>
+            {
+                isEditing ? <textarea defaultValue={article.content} onChange={(e) => setEditContent(e.target.value)}></textarea> : <p>{article.content}</p>
+            }
+            
             <div className="btn-container">
-                <button>Editer</button>
+                {isEditing ? 
+                    <button onClick={() => handleEdit()}>Valider</button> :
+                    <button onClick={() => setIsEditing(true)}>Editer</button> 
+                }
                 <button>Supprimer</button>
             </div>
         </div>
